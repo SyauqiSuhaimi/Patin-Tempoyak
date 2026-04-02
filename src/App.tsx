a93d796d4fabe9e4646c9b2bfc14a8fc617626b9
SyauqiSuhaimi <97452632+SyauqiSuhaimi@users.noreply.github.com>
2026-04-02 15:49:41 +0800

    Comment out SeasonalBanner and RamadhanPromo
    Temporarily disabled SeasonalBanner and RamadhanPromo in src/App.tsx by commenting out their JSX usage to stop rendering them (useful for debugging or staging). Also normalized import strings to use double quotes and cleaned up minor leading whitespace.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { RamadhanPromo } from './components/RamadhanPromo';
-import { About } from './components/About';
-import { Menu } from './components/Menu';
-import { Gallery } from './components/Gallery';
-import { Reviews } from './components/Reviews';
-import { Footer } from './components/Footer';
-import { SeasonalBanner } from './components/SeasonalBanner';
-import { SeoHead } from './components/SeoHead';
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { RamadhanPromo } from "./components/RamadhanPromo";
+import { About } from "./components/About";
+import { Menu } from "./components/Menu";
+import { Gallery } from "./components/Gallery";
+import { Reviews } from "./components/Reviews";
+import { Footer } from "./components/Footer";
+import { SeasonalBanner } from "./components/SeasonalBanner";
+import { SeoHead } from "./components/SeoHead";
 
 function App() {
   return (
     <div className="min-h-screen font-sans bg-secondary">
       <SeoHead />
       <div className="fixed top-0 left-0 right-0 z-50 flex flex-col">
-        <SeasonalBanner />
+        {/* <SeasonalBanner /> */}
         <Navbar />
       </div>
       <main>
         <Hero />
-        <RamadhanPromo />
+        {/* <RamadhanPromo /> */}
         <About />
         <Menu />
         <Gallery />
